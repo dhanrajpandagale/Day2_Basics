287e5ab7fcc201c4ab7e282ff8f857f8d5e0129e
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-07-04 02:34:39 -0400

    Read a Number

---
+++ src/Day2/IfElse2.java
@@ -0,0 +1,31 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class IfElse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a number");
+		Scanner ref = new Scanner(System.in);
+		int num = ref.nextInt();
+		ref.close();
+		
+		if (num == 1) {
+			System.out.println("Unit");
+		}
+		else if (num == 10) {
+			System.out.println("Ten");
+		}
+		else if (num == 100) {
+			System.out.println("Hundred");
+		}
+		else if (num == 1000) {
+			System.out.println("Thousand");
+		}
+		else  {
+			System.out.println("Invalid Input");
+		}
+	}
+
+}
