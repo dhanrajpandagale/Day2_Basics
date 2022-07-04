4ae6e8762f8802f302384bc69512abe0b66a511d
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-07-04 02:52:17 -0400

    Reverse the Integer Number in For Loop

---
+++ src/Day2/ForLoop2.java
@@ -0,0 +1,22 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class ForLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int rev= 0, rem=0;
+		
+		System.out.println("Enter any number ");
+		Scanner sc = new Scanner(System.in);
+				
+		for ( int n = sc.nextInt(); n>0; n=n/10) {
+			 rem= n%10;
+			 rev= rev*10+ rem;
+     }
+				
+		System.out.print(rev);
+	}
+
+}
